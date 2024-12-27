83eaa018dae6250943bc15dc984d81157f33ae05
Stephen Molloy <sdmolloy@gmail.com>
2024-12-27 23:16:49 +0100

    Prepping for quads

--- src/bundle/acc_elements.c
+++ src/bundle/acc_elements.c
@@ -3,3 +3,11 @@
 Drift make_drift(float len) {
   return (Drift){ .length = len };
 }
+
+Quad make_quad(float len, float K1) {
+  return (Quad){
+    .length = len,
+    .K1 = K1,
+  };
+}
+
--- src/bundle/acc_elements.h
+++ src/bundle/acc_elements.h
@@ -5,7 +5,13 @@ typedef struct {
   double length;
 } Drift;
 
+typedef struct {
+  double length;
+  double K1;
+} Quad;
+
 Drift make_drift(float len);
+Quad make_quad(float len, float K1);
 
 #endif // !_ACC_ELEMENTS_H
 
