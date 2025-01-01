f4ab48dcad1cf36096d58f720670d5c7aae85bec
Stephen Molloy <sdmolloy@gmail.com>
2025-01-02 00:34:57 +0100

    Unused code

--- src/main.c
+++ src/main.c
@@ -16,12 +16,76 @@ static sdm_arena_t *active_arena = &main_arena;
 void *active_alloc(size_t size)              { return sdm_arena_alloc(active_arena, size); }
 void *active_realloc(void *ptr, size_t size) { return sdm_arena_realloc(active_arena, ptr, size); }
 
+// VarType get_binop_result_type(BinOp *binop) {
+//   ASTNode *lhs = binop->lhs;
+//   ASTNode *rhs = binop->rhs;
+//
+//   VarType lhs_type, rhs_type;
+//   switch (lhs->type) {
+//     case NODE_TYPE_BINOP: {
+//       lhs_type = get_binop_result_type(&lhs->as.binop);
+//     } break;
+//     case NODE_TYPE_LITERAL: {
+//       lhs_type = lhs->as.literal.type;
+//     } break;
+//     case NODE_TYPE_VARIABLE: {
+//       lhs_type = lhs->as.variable
+//     } break;
+//     case NODE_TYPE_FUNCALL: {} break;
+//     case NODE_TYPE_VARINIT:
+//     case NODE_TYPE_COUNT: {
+//       fprintf(stderr, "Reached the unreachable\n");
+//       exit(1);
+//     } break;
+//   }
+// }
+
+void check_astnode(ASTNode *ast) {
+  char *astnode_type_strings[] = {
+    [NODE_TYPE_BINOP]    = "NODE_TYPE_BINOP",
+    [NODE_TYPE_LITERAL]  = "NODE_TYPE_LITERAL",
+    [NODE_TYPE_VARIABLE] = "NODE_TYPE_VARIABLE",
+    [NODE_TYPE_VARINIT]  = "NODE_TYPE_VARINIT",
+    [NODE_TYPE_FUNCALL]  = "NODE_TYPE_FUNCALL",
+  };
+  static_assert(sizeof(astnode_type_strings) / sizeof(astnode_type_strings[0]) == NODE_TYPE_COUNT,
+                "You forgot to extend 'astnode_type_strings'\n");
+
+  switch (ast->type) {
+    case NODE_TYPE_BINOP:    {
+      fprintf(stderr, "Unable to check nodes of type '%s'\n", astnode_type_strings[ast->type]);
+      exit(1);
+    } break;
+    case NODE_TYPE_LITERAL:  {
+      fprintf(stderr, "Unable to check nodes of type '%s'\n", astnode_type_strings[ast->type]);
+      exit(1);
+    } break;
+    case NODE_TYPE_VARINIT:  {
+      fprintf(stderr, "Unable to check nodes of type '%s'\n", astnode_type_strings[ast->type]);
+      exit(1);
+    } break;
+    case NODE_TYPE_VARIABLE: {
+      fprintf(stderr, "Unable to check nodes of type '%s'\n", astnode_type_strings[ast->type]);
+      exit(1);
+    } break;
+    case NODE_TYPE_FUNCALL:  {
+      fprintf(stderr, "Unable to check nodes of type '%s'\n", astnode_type_strings[ast->type]);
+      exit(1);
+    } break;
+    case NODE_TYPE_COUNT: {
+      fprintf(stderr, "Reached the unreachable. Bug in the AST generator");
+      exit(1);
+    }
+  }
+}
+
 int main(void) {
   variable_lib = NULL;
   builtin_func_sigs = define_builtin_funcs();
 
-  char *input_filename = "small_example.ll";
+  // char *input_filename = "small_example.ll";
   // char *input_filename = "example.ll";
+  char *input_filename = "bracket_example.ll";
   // char *input_filename = "type_example.ll";
   char *buffer = sdm_read_entire_file(input_filename);
 
